31af70ac5db60aebaca4e1ace028539fe1401b73
Surdum <31803702+Surdum@users.noreply.github.com>
2018-02-04 15:23:16 +0300

    мод
    поверхносная проверка

--- SEO_text_analysis/load_data.py
+++ SEO_text_analysis/load_data.py
@@ -1,3 +1,11 @@
 # функция загрузки текста из файла
 # input path
 # output variable with text
+
+
+def getText():
+    print('Имя файла: ')
+    # file = open(input(), 'r')
+    file = open('text.txt', 'r')
+    t = file.read()
+    return t
--- SEO_text_analysis/main.py
+++ SEO_text_analysis/main.py
@@ -1,1 +1,38 @@
 from load_data import *
+
+vocab = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЁЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' \
+                'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# 0. получить текст
+stext = getText()
+
+# 1. заменить символы переноса на пробелы
+ltext = list()
+ltext += [lit if ord(lit) != 10 else ' ' for lit in stext]
+stext = ''.join(ltext)
+
+# 2. посчитать пробелы и буквы
+ispaces = 0
+ilits = 0
+for lit in stext:
+    if ord(lit) == 32:
+        ispaces += 1
+    if lit in vocab:
+        ilits += 1
+
+
+# 3. разделить на слова и удалить небуквы
+lwords = stext.split(' ')
+bufwords = list()  # временно
+bufwords.extend(lwords)
+
+
+# вывод
+for i in stext:
+    print(i, ord(i))
+print(bufwords)
+print()
+print('Символов:                ', len(stext))
+print('Символов ( без пробелов):', len(stext) - ispaces)
+print('Букв:                    ', ilits)
+print('Слов:                    ', len(bufwords))
