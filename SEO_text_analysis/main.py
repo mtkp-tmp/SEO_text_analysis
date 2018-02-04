c21218394e76f767e28fd38c11cdad38f1179725
Surdum <31803702+Surdum@users.noreply.github.com>
2018-02-04 16:27:29 +0300

    изм текст
    добавил лишних пробелов

--- SEO_text_analysis/main.py
+++ SEO_text_analysis/main.py
@@ -11,28 +11,32 @@ ltext = list()
 ltext += [lit if ord(lit) != 10 else ' ' for lit in stext]
 stext = ''.join(ltext)
 
-# 2. посчитать пробелы и буквы
+# 2. удалить лишние пробелы и разделить на слова
+lwords = stext.split()
+stext = ' '.join(lwords)
+bufwords = list()  # для сравнения изменений
+bufwords.extend(lwords)
+
+# 3. посчитать пробелы и буквы
 ispaces = 0
 ilits = 0
 for lit in stext:
-    if ord(lit) == 32:
-        ispaces += 1
     if lit in vocab:
         ilits += 1
+    elif ord(lit) == 32:
+        ispaces += 1
 
-
-# 3. разделить на слова и удалить небуквы
-lwords = stext.split(' ')
-bufwords = list()  # временно
-bufwords.extend(lwords)
-
+# 4. удалить небуквы и неслова
+newlwords = list()
+newlwords = [word.lower() if word[len(word)-1] in vocab else word[0:-1].lower() for word in lwords]
 
 # вывод
 for i in stext:
     print(i, ord(i))
 print(bufwords)
+print(newlwords)
 print()
-print('Символов:                ', len(stext))
+print('Символов:                ', len(stext))  # 156 прав
 print('Символов ( без пробелов):', len(stext) - ispaces)
 print('Букв:                    ', ilits)
 print('Слов:                    ', len(bufwords))
