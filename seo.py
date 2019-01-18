d617bb862e7cf5dd2531528b5c13efeb3b4a9084
Seizz <36148013+Seizz@users.noreply.github.com>
2019-01-18 23:09:34 +0300

    Update seo.py

--- seo.py
+++ seo.py
@@ -3,11 +3,10 @@ data = f.read().lower()
 print('Количество символов текста (с пробелами) : ' + str(len(data)))
 print('Количество символов текста (без пробелов) : ' + str(len(data.replace(' ', ''))))
 print('Количество букв в тексте : ' + str(len([i for i in data if i.isalpha()])))
-print('Количество слов в тексте : ' + str(len(data.split())))
+print('Количество слов в тексте : ' + str(len([w for w in ''.join([l  if l.isalpha() | l.isspace() else ' ' for l in data]).split()])))
 print('Самая часто-встречающаяся буква : ' + 
 	str([key + ' встречается в файле ' + str(value) + ' раз' for key, value in sorted(dict(zip([i for i in data if i.isalpha()], 
 		[data.count(i) for i in data if i.isalpha()])).items(), key=lambda item: (item[1], item[0]))][-1]))
-print(['Самое часто-встречающееся слово : ' + key + ' встречается в файле ' + str(value) + ' раз' for key, value in sorted(dict(zip(list(set([i if i.isalpha() else [i for i in list(set([i.replace(punc, '') for punc in '!"#$%&' + '()*+,-./:;<=>?@[\]^_`{|}~'])) if i.isalpha()][0] for i in data.split()])),
-			list(set([[i if i.isalpha() else [i for i in list(set([i.replace(punc, '') for punc in '!"#$%&' + '()*+,-./:;<=>?@[\]^_`{|}~'])) if i.isalpha()][0] for i in data.split()].count(i) for i in [i if i.isalpha() else [i for i in list(set([i.replace(punc, '') for punc in '!"#$%&' + '()*+,-./:;<=>?@[\]^_`{|}~'])) if i.isalpha()][0] for i in data.split()]])))).items(), key=lambda item: (item[1], item[0]))][-1])
+print(['Самое часто-встречающееся слово : ' + key + ' встречается в файле ' + str(value) + ' раз' for key, value in sorted(dict(zip([w for w in ''.join([l  if l.isalpha() | l.isspace() else ' ' for l in data]).split()],
+	[[w for w in ''.join([l  if l.isalpha() | l.isspace() else ' ' for l in data]).split()].count(w) for w in [w for w in ''.join([l  if l.isalpha() | l.isspace() else ' ' for l in data]).split()]])).items(), key=lambda item: (item[1],item[0]))][-1])
 f.close()
-
